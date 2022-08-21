d0d4acfe998f05bf09c708fae2bda89b32413539
Manoel Flores Neto <manoelfnn@gmail.com>
2022-08-21 20:01:15 -0300

    Primeiro commit

---
+++ src/main/java/com/example/springjpanew/SpringJpaAuthSwaggerMapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjpanew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaAuthSwaggerMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaAuthSwaggerMapperApplication.class, args);
+	}
+
+}
